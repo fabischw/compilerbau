36405946c440f1408763a63219fe614f12ee1662
fabischw <107812436+fabischw@users.noreply.github.com>
2025-05-16 15:51:41 +0200

    add documentation for new ast; update AstType

--- src/tree/ast_type.h
+++ src/tree/ast_type.h
@@ -1,7 +1,33 @@
 #ifndef AST_TYPE.H
-    #define AST_TYPE.H
+    #define AST_TYPE_H
 
     typedef enum _AstType {
+        statement,
+        expression,
+        loop_declaration,
+        condition_if,
+        condition_content,
+        condition_elif,
+        condition_else,
+        variable_declaration,
+        datatype,
+        assignment,
+        variable_declaration_const,
+        array_declaration,
+        array_indexing,
+        logical_expression,
+        arithmetic_expression,
+        unary_expression,
+        function_call,
+        parameters,
+        INTEGER,
+        FLOAT,
+        BOOLEAN,
+        STRING,
+        CHARACTER,
+        IDENTIFIER,
+        array,
+        array_item
     } AstType;
 
 #endif
