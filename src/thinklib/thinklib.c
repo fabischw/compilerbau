ad01d968335cc1320cbd29201fd5abaa11ca4b3d
Johannes Lingk <johannes.lingk@gmail.com>
2025-05-23 11:30:26 +0200

    fix thinklib.c

--- src/thinklib/thinklib.c
+++ src/thinklib/thinklib.c
@@ -11,11 +11,11 @@ LL_Node* create_stdlib_symbol_table() {
         ll_create_dataType("print", TYP_FUNCTION, true, -1),
         ll_create_dataType("input", TYP_FUNCTION, true, -1),
     };
-    lib[0]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_FLOAT);
+    lib[0]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_INT);
     lib[1]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_STRING);
-    lib[2]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_INT);
-    lib[3]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_BOOLEAN);
-    lib[4]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_CHARACTER);
+    lib[2]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_CHARACTER);
+    lib[3]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_FLOAT);
+    lib[4]->func = func_create(1, (VarType[]){TYP_ANY}, TYP_BOOLEAN);
     lib[5]->func = func_create(0, NULL, TYP_NULL);
     lib[6]->func = func_create(1, (VarType[]){TYP_STRING}, TYP_NULL);
     lib[7]->func = func_create(1, (VarType[]){TYP_STRING}, TYP_STRING);
